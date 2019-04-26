e6560f56d4f2ee1c6362d4c3cf0c3939bc76d9ae
Scherbakov Alexandr <Smersh.Root@gmail.com>
2019-04-26 14:48:52 +0500

    Добавил скрипты инициализации БД

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,35 +1,20 @@
-create table USER
+create table RESTAURANT
 (
-  ID         INTEGER default (NEXT VALUE FOR PUBLIC.SYSTEM_SEQUENCE_5913B4FF_D458_491E_B8A1_8E9ADAAE7102),
-  LOGIN      VARCHAR(500)  not null,
-  PASSWORD   VARCHAR(4000) not null,
-  FIRST_NAME VARCHAR(500),
-  LAST_NAME  VARCHAR(500),
-  PATRONYMIC VARCHAR(500),
-  CREATE_DATE DATETIME,
-  MODIFIED_DATE DATETIME,
-  constraint USER__PK
+  ID            INTEGER      not null,
+  NAME          VARCHAR(500) not null,
+  PHONE         VARCHAR(100),
+  ADDRESS       VARCHAR(2000),
+  NOTE          VARCHAR(2000),
+  EMAIL         VARCHAR(500),
+  TIME_WORK     VARCHAR(1000),
+  CREATE_DATE   TIMESTAMP,
+  MODIFIED_DATE TIMESTAMP,
+  constraint RESTAURANT__PK
   primary key (ID)
 );
 
-comment on table USER
-is 'Пользователь';
-
-CREATE TABLE RESTAURANT
-(
-    ID int NOT NULL,
-    NAME VARCHAR2(500) NOT NULL,
-    PHONE VARCHAR2(100),
-    ADDRESS VARCHAR2(2000),
-    NOTE VARCHAR2(2000),
-    EMAIL VARCHAR2(500),
-    TIME_WORK VARCHAR2(1000),
-    CREATE_DATE DATETIME,
-    MODIFIED_DATE DATETIME,
-    constraint RESTAURANT__PK
-    primary key (ID)
-);
-COMMENT ON TABLE RESTAURANT IS 'Ресторан'
+comment on table RESTAURANT
+is 'Ресторан';
 
 create table MENU
 (
@@ -41,8 +26,11 @@ create table MENU
   RESTAURANT_ID INTEGER      not null,
   CREATE_DATE   TIMESTAMP,
   MODIFY_DATE   TIMESTAMP,
+  MODIFIED_DATE TIMESTAMP,
   constraint MENU__PK
   primary key (ID),
+  constraint FKBLWDTXEVPL4MRDS8A12Q0OHU6
+  foreign key (RESTAURANT_ID) references RESTAURANT,
   constraint MENU_RESTAURANT_ID_FK
   foreign key (RESTAURANT_ID) references RESTAURANT
   on update cascade on delete cascade
@@ -50,3 +38,41 @@ create table MENU
 
 comment on table MENU
 is 'Меню';
+
+create table USER
+(
+  ID               INTEGER default (NEXT VALUE FOR PUBLIC.SYSTEM_SEQUENCE_5913B4FF_D458_491E_B8A1_8E9ADAAE7102),
+  LOGIN            VARCHAR(500)  not null,
+  PASSWORD         VARCHAR(4000) not null,
+  FIRST_NAME       VARCHAR(500),
+  LAST_NAME        VARCHAR(500),
+  PATRONYMIC       VARCHAR(500),
+  CREATE_DATE      TIMESTAMP,
+  MODIFIED_DATE    TIMESTAMP,
+  SECURITY_ROLE_ID INTEGER,
+  constraint USER__PK
+  primary key (ID)
+);
+
+comment on table USER
+is 'Пользователь';
+
+create table VOTING
+(
+  ID            INTEGER default (NEXT VALUE FOR PUBLIC.SYSTEM_SEQUENCE_D2CD4B47_0643_4B9E_9BF1_393ECBD22B56)
+    primary key,
+  RESTAURANT_ID INTEGER not null,
+  USER_ID       INTEGER,
+  CREATE_DATE   TIMESTAMP,
+  MODIFIED_DATE TIMESTAMP,
+  REPORT_DATE   DATE    not null,
+  constraint VOTING_RESTAURANT_ID_FK
+  foreign key (RESTAURANT_ID) references RESTAURANT
+  on update cascade on delete cascade,
+  constraint VOTING_USER_ID_FK
+  foreign key (USER_ID) references USER
+  on update cascade on delete set null
+);
+
+comment on table VOTING
+is 'Голосование пользователей за рестораны';
