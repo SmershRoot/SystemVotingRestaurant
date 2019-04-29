42c21e14ec5404f64d9bdcd03803bdae6401a629
Scherbakov Alexandr <Smersh.Root@gmail.com>
2019-04-29 11:55:32 +0500

    Удалил непонятно откуда взявшиеся копии файлов

--- repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.smv.system.restaurant.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import ru.smv.system.restaurant.models.db.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-
-}
